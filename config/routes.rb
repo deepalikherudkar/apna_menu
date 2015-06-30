ad73148041b1ff7f42ce856fe62df0723c5b7a2c
Deepali <deepali@bangthetable.com>
2015-06-30 15:37:33 +0530

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,8 +11,10 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  
   get 'home' => 'home#index'
   resources :home
+
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
